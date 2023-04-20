6e675007c56115fa81d502878ee121b5d6fbf954
Alina Akulina <akulina.alina1999@gmail.com>
2023-04-20 13:10:00 +0000

    Repeta practice

---
+++ js/main.js
@@ -0,0 +1,85 @@
+
+// const age = 10;
+// const totalPrice = 200.74;
+// const userName = 'Chelcy';
+// const message = 'Добро пожаловать';
+// const isOpen = true;
+// const shouldConfirm = false;
+
+// console.log(totalPrice);
+
+// const type = typeof isOpen;
+// console.log(type);
+
+// alert('jdfidjiod');
+
+
+// Ввод пользователя
+// const shouldRenew = confirm('Хотите продлить подписку?');
+// console.log(shouldRenew);
+
+
+// Ввод колличества товара
+// let quantity = prompt('Ведите колличество товара');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+
+// Числа  Number ParseInt() and parseFloat()
+// let elementWidth = '50.25px';
+// elementWidth = Number.parseInt(elementWidth);
+// console.log('elementWidth: ', elementWidth);
+
+// let elementHeight = '200.74px';
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log('elementHeight: ', elementHeight)
+
+
+// toFixed
+// let salary = 1300.16472;
+// salary = Number(salary.toFixed(2));
+// console.log(salary);
+// console.log(Number(salary.toFixed(2)));
+
+
+// NaN
+// let quantity = '30';
+// let value = 'Эту строку невозможно привести к числу'
+// console.log(Number(quantity));
+// console.log(Number(value));
+
+
+// Math
+// const base = 2;
+// const power = 5;
+
+// const result = Math.pow(base, power);
+// console.log(result);
+// console.log(base ** power);
+
+
+// Степень, данные и т.п
+// let base = prompt('Давай число');
+// base = Number(base);
+// console.log(base);
+
+// let power = prompt('Давай степень');
+// power = Number(power);
+// console.log(power);
+
+// const result = base ** power;
+// console.log(result);
+
+
+// Генерация чисел Math.round Math.random
+// const max = 80;
+// const min = 20;
+// const result = Math.round(Math.random() * (max - min) + min);
+// console.log(result);
+// (Math.random() * (max - min) + min);
+
+// length длина строки
+const message = 'В этой строке 26 символов';
+console.log(message.length);
+
