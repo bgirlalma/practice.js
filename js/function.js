43058e9daed1be5943a24a428ef1f88cfc9a1e43
Alina Akulina <akulina.alina1999@gmail.com>
2023-04-28 23:02:04 +0000

    функции/Репета

---
+++ js/function.js
@@ -0,0 +1,39 @@
+// Функциональные выражения; Аргументы и параметры; Возвращение значения 
+    // const add = function (x, y) {
+    // console.log('Выполняеться функция add');
+    // return x + y;
+    // }
+
+    // const r1 = add(5, 3);
+    // console.log('r1: ', r1);
+
+    // const r2 = add (10, 15);
+    // console.log('r2: ', r2);
+
+    //     // 
+    // const fn = function (){
+    //     console.log(1);
+    //     console.log(2);
+    //     console.log(3); 
+    //     // "Где ты поставишь Return там и заканчивается  исполненение функции"
+    //     return;
+    // }
+    // console.log(fn());
+
+        // 
+    // const fn = function (value){
+
+    //     if (value < 50){
+    //         return "Меньше чем 50";
+    //     }
+
+    //     return "Больше чем 50";
+    // }
+    // console.log('Результат функции: ', fn(10));
+    // console.log('Результат функции: ', fn(10000));
+
+
+// Стек вызовов; Stack trace и поиск ошибок
+
+
+        
